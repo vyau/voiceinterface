671a6feab1465f9efe2fd87ebbfb62a378a4c2c7
Vincent Yau <vincent.yau@gmail.com>
2015-03-01 10:19:40 -0800

    initial check-in, so william can do integration

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # voiceinterface
 voice recognition and text to speech
+
+Dependencies:
+
+   -- Maven build tool (on Ubuntu, apt-get install maven)
+   -- Recent version of Java (Download from Oracle Java website, I am using Java 8)  
+   -- vlc media player (on Ubuntu, apt-get install vlc)
+   -- wget (on Ubuntu, apt-get install wget)
+
+
+To build:
+
+   -- cd to the directory with pom.xml,  type "mvn -amd install"
+  
+To run:
+
+   java -cp sphinx4-samples-1.0-SNAPSHOT.jar -cp ./classes -cp sphinx4-samples-1.0-SNAPSHOT-jar-with-dependencies.jar edu.cmu.sphinx.demo.dialog.DialogDemo
+
+
+To clean:
+
+   mvn -amd clean
+
+
---
+++ src/main/java/edu/cmu/sphinx/demo/dialog/DialogDemo.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright 2013 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ */
+
+package edu.cmu.sphinx.demo.dialog;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.StringBuffer;
+import java.lang.Thread;
+
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+
+
+public class DialogDemo {
+
+    private static final String ACOUSTIC_MODEL =
+        "resource:/edu/cmu/sphinx/models/en-us/en-us";
+    private static final String DICTIONARY_PATH =
+        "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
+    private static final String GRAMMAR_PATH =
+        "resource:/edu/cmu/sphinx/demo/dialog/";
+    private static final String LANGUAGE_MODEL =
+        "resource:/edu/cmu/sphinx/demo/dialog/2343.lm";
+
+    private static final Map<String, Integer> DIGITS =
+        new HashMap<String, Integer>();
+
+
+    final static String[] hex = {
+    "%00", "%01", "%02", "%03", "%04", "%05", "%06", "%07",
+    "%08", "%09", "%0a", "%0b", "%0c", "%0d", "%0e", "%0f",
+    "%10", "%11", "%12", "%13", "%14", "%15", "%16", "%17",
+    "%18", "%19", "%1a", "%1b", "%1c", "%1d", "%1e", "%1f",
+    "%20", "%21", "%22", "%23", "%24", "%25", "%26", "%27",
+    "%28", "%29", "%2a", "%2b", "%2c", "%2d", "%2e", "%2f",
+    "%30", "%31", "%32", "%33", "%34", "%35", "%36", "%37",
+    "%38", "%39", "%3a", "%3b", "%3c", "%3d", "%3e", "%3f",
+    "%40", "%41", "%42", "%43", "%44", "%45", "%46", "%47",
+    "%48", "%49", "%4a", "%4b", "%4c", "%4d", "%4e", "%4f",
+    "%50", "%51", "%52", "%53", "%54", "%55", "%56", "%57",
+    "%58", "%59", "%5a", "%5b", "%5c", "%5d", "%5e", "%5f",
+    "%60", "%61", "%62", "%63", "%64", "%65", "%66", "%67",
+    "%68", "%69", "%6a", "%6b", "%6c", "%6d", "%6e", "%6f",
+    "%70", "%71", "%72", "%73", "%74", "%75", "%76", "%77",
+    "%78", "%79", "%7a", "%7b", "%7c", "%7d", "%7e", "%7f",
+    "%80", "%81", "%82", "%83", "%84", "%85", "%86", "%87",
+    "%88", "%89", "%8a", "%8b", "%8c", "%8d", "%8e", "%8f",
+    "%90", "%91", "%92", "%93", "%94", "%95", "%96", "%97",
+    "%98", "%99", "%9a", "%9b", "%9c", "%9d", "%9e", "%9f",
+    "%a0", "%a1", "%a2", "%a3", "%a4", "%a5", "%a6", "%a7",
+    "%a8", "%a9", "%aa", "%ab", "%ac", "%ad", "%ae", "%af",
+    "%b0", "%b1", "%b2", "%b3", "%b4", "%b5", "%b6", "%b7",
+    "%b8", "%b9", "%ba", "%bb", "%bc", "%bd", "%be", "%bf",
+    "%c0", "%c1", "%c2", "%c3", "%c4", "%c5", "%c6", "%c7",
+    "%c8", "%c9", "%ca", "%cb", "%cc", "%cd", "%ce", "%cf",
+    "%d0", "%d1", "%d2", "%d3", "%d4", "%d5", "%d6", "%d7",
+    "%d8", "%d9", "%da", "%db", "%dc", "%dd", "%de", "%df",
+    "%e0", "%e1", "%e2", "%e3", "%e4", "%e5", "%e6", "%e7",
+    "%e8", "%e9", "%ea", "%eb", "%ec", "%ed", "%ee", "%ef",
+    "%f0", "%f1", "%f2", "%f3", "%f4", "%f5", "%f6", "%f7",
+    "%f8", "%f9", "%fa", "%fb", "%fc", "%fd", "%fe", "%ff"
+    };
+
+
+    static {
+        DIGITS.put("oh", 0);
+        DIGITS.put("zero", 0);
+        DIGITS.put("one", 1);
+        DIGITS.put("two", 2);
+        DIGITS.put("three", 3);
+        DIGITS.put("four", 4);
+        DIGITS.put("five", 5);
+        DIGITS.put("six", 6);
+        DIGITS.put("seven", 7);
+        DIGITS.put("eight", 8);
+        DIGITS.put("nine", 9);
+    }
+
+
+    public static String encode(String s)
+    {
+        StringBuffer sbuf = new StringBuffer();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            int ch = s.charAt(i);
+            if ('A' <= ch && ch <= 'Z') {     // 'A'..'Z'
+                sbuf.append((char)ch);
+            } else if ('a' <= ch && ch <= 'z') {  // 'a'..'z'
+                sbuf.append((char)ch);
+            } else if ('0' <= ch && ch <= '9') {  // '0'..'9'
+                sbuf.append((char)ch);
+            } else if (ch == ' ') {           // space
+                sbuf.append('+');
+            } else if (ch == '-' || ch == '_'     // unreserved
+            || ch == '.' || ch == '!'
+            || ch == '~' || ch == '*'
+            || ch == '\'' || ch == '('
+            || ch == ')') {
+                sbuf.append((char)ch);
+            } else if (ch <= 0x007f) {        // other ASCII
+                sbuf.append(hex[ch]);
+            } else if (ch <= 0x07FF) {        // non-ASCII <= 0x7FF
+                sbuf.append(hex[0xc0 | (ch >> 6)]);
+                sbuf.append(hex[0x80 | (ch & 0x3F)]);
+            } else {                  // 0x7FF < ch <= 0xFFFF
+                sbuf.append(hex[0xe0 | (ch >> 12)]);
+                sbuf.append(hex[0x80 | ((ch >> 6) & 0x3F)]);
+                sbuf.append(hex[0x80 | (ch & 0x3F)]);
+            }
+        }
+        return sbuf.toString();
+    }
+
+
+    private static String executeCommand(String command) {
+ 
+        StringBuffer output = new StringBuffer();
+ 
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = 
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+ 
+                        String line = "";           
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+            p.destroy();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+ 
+
+    private static double parseNumber(String[] tokens) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < tokens.length; ++i) {
+            if (tokens[i].equals("point"))
+                sb.append(".");
+            else
+                sb.append(DIGITS.get(tokens[i]));
+        }
+        return Double.parseDouble(sb.toString());
+    }
+    
+
+
+    private static void recognizerBankAccount(LiveSpeechRecognizer recognizer) {
+        System.out.println("This is bank account voice menu");
+        System.out.println("-------------------------------");
+        System.out.println("Example: balance");
+        System.out.println("Example: withdraw zero point five");
+        System.out.println("Example: deposit one two three");
+        System.out.println("Example: back");
+        System.out.println("-------------------------------");
+
+        double savings = .0;
+        recognizer.startRecognition(true);
+
+        while (true) {
+            String utterance = recognizer.getResult().getHypothesis();
+            if (utterance.endsWith("back")) {
+                break;
+            } else if (utterance.startsWith("deposit")) {
+                double deposit = parseNumber(utterance.split("\\s"));
+                savings += deposit;
+                System.out.format("Deposited: $%.2f\n", deposit);
+            } else if (utterance.startsWith("withdraw")) {
+                double withdraw = parseNumber(utterance.split("\\s"));
+                savings -= withdraw;
+                System.out.format("Withdrawn: $%.2f\n", withdraw);
+            } else if (!utterance.endsWith("balance")) {
+                System.out.println("Unrecognized command: " + utterance);
+            }
+
+            System.out.format("Your savings: $%.2f\n", savings);
+        }
+
+        recognizer.stopRecognition();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.setAcousticModelPath(ACOUSTIC_MODEL);
+        configuration.setDictionaryPath(DICTIONARY_PATH);
+        configuration.setGrammarPath(GRAMMAR_PATH);
+        configuration.setUseGrammar(true);
+
+        /*
+        configuration.setGrammarName("dialog");
+        LiveSpeechRecognizer jsgfRecognizer =
+            new LiveSpeechRecognizer(configuration);
+
+        configuration.setGrammarName("digits.grxml");
+        LiveSpeechRecognizer grxmlRecognizer =
+            new LiveSpeechRecognizer(configuration);
+            */
+
+
+        configuration.setUseGrammar(false);
+        configuration.setLanguageModelPath(LANGUAGE_MODEL);
+        //LiveSpeechRecognizer lmRecognizer =
+        //    new LiveSpeechRecognizer(configuration);
+
+        //lmRecognizer.startRecognition(true);
+        while (true) {
+            LiveSpeechRecognizer lmRecognizer =
+                new LiveSpeechRecognizer(configuration);
+            lmRecognizer.startRecognition(true);
+            System.out.println("\n\nstarting recognition");
+
+            String utterance = lmRecognizer.getResult().getHypothesis();
+            System.out.println(">>" + utterance + "<<");
+            lmRecognizer.stopRecognition();
+            if (utterance.startsWith("exit")) {
+                break;
+            }
+
+            if (utterance.length() > 0) {
+                
+                String results = executeCommand("ls -l");
+                System.out.println(results);
+                System.out.println("\n");
+                
+                String tts = encode("Received command " + utterance);
+                
+
+                PrintWriter writer = new PrintWriter("/tmp/the-file-name.txt", "UTF-8");
+                writer.println("/usr/bin/wget -q -U Mozilla " +
+                               " \"http://translate.google.com/translate_tts?tl=en&q=" + 
+                                tts + "\" " +
+                                " -O - | vlc --play-and-exit -");
+                
+                writer.close();
+                String res = executeCommand("/bin/bash /tmp/the-file-name.txt");
+                Thread.sleep(600);
+                
+            }
+            /*
+            
+            if (utterance.equals("bank account")) {
+                jsgfRecognizer.stopRecognition();
+                recognizerBankAccount(jsgfRecognizer);
+                jsgfRecognizer.startRecognition(true);
+            }
+
+            */
+        }
+    }
+    //lmRecognizer.stopRecognition();
+}
